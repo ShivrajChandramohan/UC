cb7dcbece8d22571c247abe68e05b3c5ed6ae826
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-09 11:11:59 +0530

    Attendence

---
+++ UCAssignments/UC1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCAssignments
+{
+    public class UC1
+    {
+            public static void CheckAttendance()
+            {
+                int Full_Time = 1;
+
+                Random random = new Random();
+                int empcheck = random.Next(2);
+
+                if (empcheck == Full_Time)
+                {
+                    Console.WriteLine("Employee is Present");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is Absent");
+                }
+            }
+      
+    }
+}
+
