65888ce44d70fb16c3cfb5d8b0b69feddb7a79d9
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-09 11:14:35 +0530

    Partwage

---
+++ UCAssignments/UC3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCAssignments
+{
+    public class UC3
+    {
+        public static void Parttimewage()
+        {
+            int part_time = 1;
+            int emp_rate_per_hr = 20;
+            int emphr = 0;
+            int empwage = 0;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+
+            if (empcheck == part_time)
+            {
+                Console.WriteLine("Employee is Present");
+                emphr = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                emphr = 0;
+            }
+
+            empwage = emphr * emp_rate_per_hr;
+            Console.WriteLine("Empwage " + empwage);
+        }
+    }
+}
